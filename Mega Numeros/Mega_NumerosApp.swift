2396cf2a6af8c0b8c4f8b502d60026e47ae42004
Leonardo <fernandes.leo@live.com>
2023-09-09 00:51:43 -0300

    Initial Commit

---
+++ Mega Numeros/Mega_NumerosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mega_NumerosApp.swift
+//  Mega Numeros
+//
+//  Created by Leonardo Fernandes on 09/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Mega_NumerosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
