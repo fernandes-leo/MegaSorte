f94ce664994c03908e300d5d0446cbcdfe56360c
Leonardo <fernandes.leo@live.com>
2023-09-11 22:10:51 -0300

    11/09

--- Mega Numeros/ContentView.swift
+++ Mega Numeros/ContentView.swift
@@ -6,16 +6,106 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct ContentView: View {
+    @State private var numerosSorteados: [Int] = []
+    @State private var sorteando = false
+    @State private var showingWebView = false
+    @State private var webURL = "https://loterias.caixa.gov.br/Paginas/App/Mega-Sena.aspx"
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.white.edgesIgnoringSafeArea(.all)
+
+            VStack {
+                Text("Mega-Sena")
+                    .font(.largeTitle)
+                    .foregroundColor(.blue)
+                    .padding()
+
+                if numerosSorteados.count == 6 {
+                    Text("Números Sorteados:")
+                        .font(.headline)
+                        .foregroundColor(.blue)
+
+                    LazyVGrid(columns: [GridItem(), GridItem(), GridItem()]) {
+                        ForEach(numerosSorteados, id: \.self) { numero in
+                            Circle()
+                                .foregroundColor(.blue)
+                                .frame(width: 50, height: 50)
+                                .overlay(
+                                    Text("\(numero)")
+                                        .font(.title)
+                                        .foregroundColor(.white)
+                                )
+                                .padding(10)
+                        }
+                    }
+                    .transition(.move(edge: .bottom))
+                } else {
+                    Text("Pressione para sortear")
+                        .font(.headline)
+                        .foregroundColor(.blue)
+                        .padding()
+                        .opacity(sorteando ? 0 : 1)
+                        .transition(.opacity)
+                }
+
+                Button(action: {
+                    sorteando.toggle()
+                    self.sortearNumeros()
+                }) {
+                    Text(sorteando ? "Sorteando..." : "Sortear Números")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.blue, lineWidth: 2)
+                        )
+                        .shadow(color: .blue, radius: 5, x: 0, y: 5)
+                        .disabled(sorteando)
+                }
+                .padding()
+                .transition(.scale)
+
+                Button(action: {
+                    showingWebView.toggle()
+                }) {
+                    Text("Ver Resultado e Prêmio")
+                        .font(.headline)
+                        .foregroundColor(.blue)
+                        .padding()
+                        .opacity(numerosSorteados.count == 6 ? 1 : 0)
+                        .transition(.opacity)
+                }
+                .sheet(isPresented: $showingWebView) {
+                    NavigationView {
+                        WebView(urlString: webURL)
+                            .navigationBarTitle("Resultado")
+                            .navigationBarItems(trailing: Button("Fechar") {
+                                showingWebView.toggle()
+                            })
+                    }
+                }
+            }
         }
-        .padding()
+    }
+
+    func sortearNumeros() {
+        DispatchQueue.global(qos: .background).async {
+            var numeros = Array(1...60)
+            numeros.shuffle()
+            let numerosSorteados = Array(numeros.prefix(6))
+            self.numerosSorteados = numerosSorteados.sorted()
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                sorteando.toggle()
+            }
+        }
     }
 }
 
---
+++ Mega Numeros/WebView.swift
@@ -0,0 +1,25 @@
+//
+//  WebView.swift
+//  Mega Numeros
+//
+//  Created by Leonardo Fernandes on 10/09/23.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let urlString: String
+
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        return webView
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let url = URL(string: urlString) {
+            let request = URLRequest(url: url)
+            uiView.load(request)
+        }
+    }
+}
